08d11eb3aac9654ab1c75886e80314793097f1f6
suraj171299 <surajpadalkar1999@gmail.com>
2024-08-24 19:02:26 +0530

    Updated user model,routes and controller logic

---
+++ backend/controllers/user.js
@@ -0,0 +1,93 @@
+import User from "../models/user.js";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export const signup = async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+        if (!username || !email || !password) {
+            return res.status(401).json({
+                message: "username or email or password is missing",
+                success: false,
+            })
+        }
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(401).json({
+                message: "User with this email already exists",
+                success: false,
+            })
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        await User.create({
+            username,
+            email,
+            password: hashedPassword
+        })
+        return res.status(201).json({
+            message: "Account created successfully",
+            success: true,
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        if (!email || !password) {
+            return res.status(401).json({
+                message: "email or password is missing",
+                success: false,
+            })
+        }
+
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(401).json({
+                message: "Incorrect email or password",
+                success: false,
+            })
+        }
+
+        const isPassword = await bcrypt.compare(password, user.password)
+
+        if(!isPassword){
+            return res.status(401).json({
+                message: "Incorrect email or password",
+                success: false,
+            })
+        }
+
+        user={
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            posts: user.posts
+        }
+
+        const loginToken = jwt.sign({userId:user._id}, process.env.SECRET_KEY,{expiresIn: 3600})
+
+        return res.cookie('token', loginToken, {httpOnly: true}).json({
+            message: `${user.username} login successful`,
+            success: true,
+            user
+        })
+    } catch(error) {
+        console.log(error);
+        
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        return res.cookie("token", {maxAge:0}).json({
+            message: "Logged Out",
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
-    name:{
+    username:{
         type: String,
         trim: true,
-        required: [true, "First Name is required"],
+        required: [true, "Username is required"],
         maxLength: 50
     },
     email: {
@@ -23,34 +21,13 @@ const userSchema = new mongoose.Schema({
         type: String,
         trim: true,
         required: [true, 'password is required'],
-        minlength: [6, 'password must have at least (6) caracters'],
     },
-    role: {
-        type: String,
-        default: 'user'
+    posts:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
     }
 }, {timestamps: true});
 
-
-userSchema.pre('save', async function(next) {
-    const user = this;
-    if (!user.isModified('password')) {
-        next();
-    }
-    // const salt = await bcrypt.genSalt(10);
-    // user.password = await bcrypt.hash(user.password, salt);
-    this.password = await bcrypt.hash(this.password, 10)
-});
-
-userSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-}
-
-userSchema.methods.getJwtToken = function () {
-    return jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
-        expiresIn: 3600
-    });
-}
 
 const User = mongoose.model("User", userSchema);
 
